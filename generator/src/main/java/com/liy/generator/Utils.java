c21baf78ba7e8493a24cc58553ae853a49def97d
LiyLinL <vip860180@gmail.com>
2021-11-16 10:33:54 +0800

    edit

--- generator/src/main/java/com/liy/generator/Utils.java
+++ generator/src/main/java/com/liy/generator/Utils.java
@@ -5,7 +5,6 @@ import com.liy.generator.exception.ExtExpection;
 public class Utils {
 
     public static void ThrowException( String msg, Object[] objects) {
-        ExtExpection e = new ExtExpection(msg, objects);
-        throw e;
+        throw new ExtExpection(msg, objects);
     }
 }
